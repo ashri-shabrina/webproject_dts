49433bb12386878f6e6c3f9ff018a067058c9af2
ashri-shabrina <ashri.shabrina@gmail.com>
2020-10-05 04:59:41 -0700

    Halaman index.php

---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">DIGITAL TALENT</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Programming
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">PHP</a>
+            <a class="dropdown-item" href="#">AJAX</a>
+            <a class="dropdown-item" href="#">CSS</a>
+            <a class="dropdown-item" href="#">JQUERY</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Software</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+   </nav>
+   <div class="container">
+    <div class="row">
+        <div class="col-4">
+            <div class="container">
+                </br></br></br>
+                <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Selamat datang!</h5>
+                    <p class="card-text">Ini adalah halaman web untuk kelas Web Developer C Digital Talent Scholarships VSGA Politeknik Negeri Malang.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+                </div></br></br>
+                <div id="list-example" class="list-group" style="width: 18rem;">
+                    <a class="list-group-item list-group-item-action" href="#list-item-1">PHP</a>
+                    <a class="list-group-item list-group-item-action" href="#list-item-2">CSS</a>
+                    <a class="list-group-item list-group-item-action" href="#list-item-3">AJAX</a>
+                    <a class="list-group-item list-group-item-action" href="#list-item-4">JQUERY</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-8"> 
+            </br></br>
+            <div class="container">
+                <h1>DIGITAL TALENT | Form dengan Bootstrap</h1></br>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-8">
+                            <form>
+                                <div class="form-group">
+                                    <label for="nama">Nama Anda :</label>
+                                    <input class="form-control" id="nama" name="nama" placeholder="Masukkan nama">
+                                </div>
+                                <div class="form-group">
+                                    <label for="alamat">Alamat Anda :</label>
+                                    <input type="password" class="form-control" id="alamat" name="alamat" placeholder="Masukkan alamat">
+                                </div>
+                                <div class="form-group">
+                                    <label for="jeniskelamin">Jenis Kelamin :</label>
+                                    <select class="form-control" name="jk" id="jeniskelamin">
+                                    <option>Laki-laki</option>
+                                    <option>Wanita</option>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                            </form>
+                        </div>
+                        <div class="col-4">
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
